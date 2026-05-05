f459ab78b2c450963dcbe024be6fa2399a7d784e
kidjool <julianbird034@gmail.com>
2026-05-05 11:40:31 -0400

    Updated framebuffer to full 800x600 display

--- sources_1/new/data_memory.vhd
+++ sources_1/new/data_memory.vhd
@@ -8,13 +8,14 @@ port (
     ALUResult : in std_logic_vector(31 downto 0); --ALU
     WriteData : in std_logic_vector(31 downto 0); --Register File
     MemWrite : in std_logic; --Control Unit
-    ReadData : out std_logic_vector(31 downto 0)
+    ReadData : out std_logic_vector(31 downto 0);
+    disp_addr : in std_logic_vector(16 downto 0); --  0-120,000 is 17 bits of info for an 800x600 display
+    disp_data : out std_logic_vector(31 downto 0)
 );
 end data_memory;
 
 architecture Behavioral of data_memory is
-    -- The Artix-7 has plenty of space, but let's start with 256 words (1KB).
-    type ram_type is array (0 to 255) of std_logic_vector(31 downto 0);
+    type ram_type is array (0 to 119999) of std_logic_vector(31 downto 0);
     signal RAM : ram_type := (others => (others => '0'));
 begin
 
@@ -22,9 +23,13 @@ process(clk)
 begin
     if rising_edge(clk) then
         if MemWrite = '1' then
-            RAM(to_integer(unsigned(ALUResult(9 downto 2)))) <= WriteData;
+            RAM(to_integer(unsigned(ALUResult(16 downto 0)))) <= WriteData;
         end if;
-        ReadData <= RAM(to_integer(unsigned(ALUResult(9 downto 2))));
+        ReadData <= RAM(to_integer(unsigned(ALUResult(16 downto 0))));
     end if;        
 end process;
+
+disp_data <= RAM(to_integer(unsigned(disp_addr))); -- no clock needed because of vga sync
+
 end Behavioral;
+
